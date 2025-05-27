729d6d1319c813589c2443d6d5a78418681ead8d
Biscue5 <slimeliment@gmail.com>
2025-05-27 16:20:32 +0900

    formatting

---
+++ setup.py
@@ -0,0 +1,13 @@
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from setuptools import find_packages, setup
+
+setup(
+    name="egoscaler",
+    version="0.1.0",
+    install_requires=[],
+    packages=find_packages(),  # ルートから探す
+    python_requires='>=3',
+)
+
